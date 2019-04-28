84679f3d5fc33870b5fe36a553aa064581846468
Jorge <jorgecbll@gmail.com>
2019-04-27 21:57:07 -0300

    first commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import Logo from '../components/Logo'
 
 import Header from "./header"
 import "./layout.css"
@@ -25,22 +26,9 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+        {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+
+        {children}
       </>
     )}
   />
